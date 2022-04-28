78adca0e5e4a0a90f8d5ec383b3353556a700d9a
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-04-27 20:37:35 -0700

    commit tree d6303e87445714541bce37d8f06af818ebe8977c

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun(fun){
+    if (fun == undefined){
+    return 'This Saturday, I want to roller-skate!';
+    }
+    else{
+        return `This Saturday, I want to ${fun}!`
+       
+    }
+}
+
+function mondayWork(work){
+    if (work == undefined){
+        return 'This Monday, I will go to the office.';
+    }
+    else{
+        return `This Monday, I will ${work}.`;
+    }
+}
+
+function wrapAdjective(adj){
+    return function (special){
+        return `You are ${adj}${special}${adj}!`;
+    }
+    return 
+}
+
